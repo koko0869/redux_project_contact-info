2a473188005ee41180ebbab7969b60153a157ec9
kimjihun <koko0869@naver.com>
2020-03-02 18:46:00 +0900

    setting more card

--- src/components/infoList/Infos.js
+++ src/components/infoList/Infos.js
@@ -1,20 +1,8 @@
 import React from "react";
 import Card from "./Card";
-//
-import AppBar from "@material-ui/core/AppBar";
-import Button from "@material-ui/core/Button";
-import CameraIcon from "@material-ui/icons/PhotoCamera";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
-//
 const InfoItem = ({
   info,
   onToggle,
@@ -47,13 +35,6 @@ const useStyles = makeStyles(theme => ({
   icon: {
     marginRight: theme.spacing(2)
   },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6)
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4)
-  },
   cardGrid: {
     paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(8)
@@ -62,16 +43,6 @@ const useStyles = makeStyles(theme => ({
     height: "100%",
     display: "flex",
     flexDirection: "column"
-  },
-  cardMedia: {
-    paddingTop: "56.25%" // 16:9
-  },
-  cardContent: {
-    flexGrow: 1
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6)
   }
 }));
 //
@@ -93,14 +64,14 @@ const Infos = ({
       {
         //
         <Container className={classes.cardGrid} maxWidth="md">
-          <Grid container spacing={4}>
+          <Grid container spacing={5}>
             {
               (infos = infos
                 .filter(info => {
                   return info.username.indexOf(search) !== -1;
                 })
                 .map(info => (
-                  <Grid item key={info} md={4}>
+                  <Grid item key={info.id} md={4}>
                     <InfoItem
                       info={info}
                       key={info.id}
@@ -116,23 +87,6 @@ const Infos = ({
             }
           </Grid>
         </Container>
-        //
-        // (infos = infos
-        //   .filter(info => {
-        //     return info.username.indexOf(search) !== -1;
-        //   })
-        //   .map(info => (
-        //     <InfoItem
-        //       info={info}
-        //       key={info.id}
-        //       favorite={info.favorite}
-        //       onToggle={onToggle}
-        //       onRemove={onRemove}
-        //       onUpdate={onUpdate}
-        //       onChange={onChange}
-        //       form={form}
-        //     />
-        //   )))
       }
     </div>
   );
